82f0564923044306b8273f6450303dd9bfa31f95
Lafore17 <dimkagomel1@gmail.com>
2018-08-06 12:03:47 +0300

    second solve

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 window.onload = function(){
-<<<<<<< Updated upstream
 	const delay = 50;
-=======
 	let btn = document.getElementById('button');
 	btn.addEventListener('click', function(e){
 		alert('alert');
 	});
 	const delay = 500;
->>>>>>> Stashed changes
 	setInterval(function(){
 		document.getElementById("counter").innerText ++;
 	}, delay);
